6b8e1cd7cb47e82912079ea9eebfce598b65e7ce
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2024-09-26 14:47:13 -0500

    feat[animal-home]: added animal-add-edit-card.component.vue

---
+++ src/animals/components/animal-add-edit-card.component.vue
@@ -0,0 +1,25 @@
+<script>
+
+  import {Animal} from "../model/animal.entity.js";
+
+  export default {
+    name: "animal-add-edit-card",
+    components: {},
+    props:{
+      animal:Animal
+    }
+  }
+</script>
+
+<template>
+  <pv-card>
+    <template #title>
+
+    </template>
+  </pv-card>
+
+</template>
+
+<style scoped>
+
+</style>
