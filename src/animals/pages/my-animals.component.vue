6bb5b6d03c687d2f5cbaef0a6560bbcec56ccd9a
LCDP <126851048+04lcdp@users.noreply.github.com>
2024-10-18 14:29:36 -0500

    Update my-animals.component.vue

--- src/animals/pages/my-animals.component.vue
+++ src/animals/pages/my-animals.component.vue
@@ -3,97 +3,90 @@ import {AnimalApiService} from "../services/animal-api.service.js";
 import {Animal} from "../model/animal.entity.js";
 import AnimalsList from "../components/animals-list.component.vue";
 
-
-
 export default {
   name: "my-animals",
-  components:{AnimalsList},
+  components: { AnimalsList },
   title: "MyAnimal",
-  data(){
-    return{
+  data() {
+    return {
       animals: [],
-      animalsApi: new AnimalApiService()
-
-    }
+      animalsApi: new AnimalApiService(),
+    };
   },
   created() {
     this.getAllAnimals();
   },
-  methods:{
-    buildAnimalFromResponseData(animals){
-      return animals.map(animal =>{
+  methods: {
+    buildAnimalFromResponseData(animals) {
+      return animals.map((animal) => {
         return new Animal(
-            animal.id,
-            animal.id_animal,
-            animal.name,
-            animal.species,
-            animal.vaccies,
-            animal.url_iot,
-            animal.url_photo,
-            animal.id_inventory,
-            animal.location,
-            animal.hear_rate,
-            animal.temperature,
-            animal.years
-        )
-      })
+          animal.id,
+          animal.id_animal,
+          animal.name,
+          animal.species,
+          animal.vaccies,
+          animal.url_iot,
+          animal.url_photo,
+          animal.id_inventory,
+          animal.location,
+          animal.hear_rate,
+          animal.temperature,
+          animal.years
+        );
+      });
     },
 
-    getAllAnimals(){
-      this.animalsApi.getAll()
-          .then(response =>{
-            console.log("llamando al api",response.data)
-            let arrayAnimals = response.data;
-            this.animals = this.buildAnimalFromResponseData(arrayAnimals);
-            console.log("Arreglo convertido",this.animals)
-          })
-          .catch(e=>{
-            console.log("Error al llamar al api: ",e);
-          })
-    }
-  }
-}
+    getAllAnimals() {
+      this.animalsApi
+        .getAll()
+        .then((response) => {
+          console.log("llamando al api", response.data);
+          let arrayAnimals = response.data;
+          this.animals = this.buildAnimalFromResponseData(arrayAnimals);
+          console.log("Arreglo convertido", this.animals);
+        })
+        .catch((e) => {
+          console.log("Error al llamar al api: ", e);
+        });
+    },
+
+    goToAddAnimal() {
+      this.$router.push("/add-animal");
+    },
+  },
+};
 </script>
 
 <template>
-
-  <div class="p-5" style="background-color: #4ADE80; ">
+  <div class="p-5" style="background-color: #4ADE80;">
     <div>
       <h1>Mis Animales</h1>
-      <hr>
+      <hr />
     </div>
 
     <div class="flex align-items-center justify-content-center p-3">
-      <pv-button label="Agregar"></pv-button>
+      <pv-button label="Agregar" @click="goToAddAnimal"></pv-button>
     </div>
 
-    <AnimalsList :animals="animals" ></AnimalsList>
+    <AnimalsList :animals="animals"></AnimalsList>
   </div>
-
-
-
-
 </template>
 
 <style scoped>
- h1{
-   font-size: 48px;
-   font-weight: bold;
-   color: black;
-   margin: 0;
- }
- hr{
-   border: 2px solid black;
- }
- .p-button{
-   background-color: black;
-   color: white;
-   width: 250px;
-   font-size: 20px;
-   font-weight: 350;
-
- }
-
-
-
+  h1 {
+    font-size: 48px;
+    font-weight: bold;
+    color: black;
+    margin: 0;
+  }
+  hr {
+    border: 2px solid black;
+  }
+  .p-button {
+    background-color: black;
+    color: white;
+    width: 250px;
+    font-size: 20px;
+    font-weight: 350;
+  }
 </style>
