87d41c5e4a7bf2c68e352ef0e2a778475e5c1e7d
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2024-10-28 19:28:40 -0500

    feat[view-and-add-vaccine]: added components from view vaccines

---
+++ src/vaccines/components/vaccine-list-animal.component.vue
@@ -0,0 +1,58 @@
+<script>
+
+
+
+
+import CardVaccine from "./card-vaccine.component.vue";
+
+export default {
+name: "vaccine-list-animal",
+  components: {CardVaccine},
+  props:{
+  vaccines:Array,
+  }
+}
+</script>
+
+<template>
+  <div class="p-5 h-auto">
+    <pv-card>
+      <template #title>
+        <div class="flex">
+          <h1 class="mb-0">Vacunas de </h1> <h1 style="color: #4ADE80; margin-bottom: 0"> hhhhh{{}}</h1>
+        </div>
+        <hr>
+
+      </template>
+      <template #content>
+
+        <div v-for="vaccine in vaccines">
+          <card-vaccine :vaccine="vaccine"></card-vaccine>
+        </div>
+
+      </template>
+      <template #footer>
+        <div class="flex gap-3 justify-content-end p-2">
+          <router-link to="/animals">
+            <pv-button severity="danger" >Cancelar</pv-button>
+          </router-link>
+
+          <pv-button severity="success" >Agregar</pv-button>
+        </div>
+
+      </template>
+    </pv-card>
+
+  </div>
+
+
+
+  
+</template>
+
+<style scoped>
+.p-card{
+  background-color: white;
+  color: black;
+}
+</style>
---
+++ src/vaccines/pages/view-vaccines.component.vue
@@ -0,0 +1,35 @@
+<script>
+import VaccineListAnimal from "../components/vaccine-list-animal.component.vue";
+import {VaccineApiService} from "../services/vaccine-api.service.js";
+
+export default {
+name: "view-vaccines",
+  components: {VaccineListAnimal},
+  data(){
+  return {
+    vaccines:[],
+    vacccineApiService: new VaccineApiService()
+
+  }},
+  created() {
+  this.getVaccineTest();
+  },
+  methods:{
+  getVaccineTest(){
+    this.vacccineApiService.getVaccines().then(response =>{
+      this.vaccines = response.data;
+    })
+  }
+
+  }
+}
+</script>
+
+<template>
+  <vaccine-list-animal :vaccines="vaccines" class=""></vaccine-list-animal>
+  
+</template>
+
+<style scoped>
+
+</style>
